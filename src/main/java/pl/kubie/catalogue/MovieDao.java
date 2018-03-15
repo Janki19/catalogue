cf2de5369ae0039a5f1619e02190ce2a31cddec4
Radek <radoslaw86n@gmail.com>
2018-03-15 20:49:48 +0000

    Not working

---
+++ src/main/java/pl/kubie/catalogue/MovieDao.java
@@ -0,0 +1,41 @@
+package pl.kubie.catalogue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class MovieDao {
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
+
+    public MovieDao() {
+         entityManagerFactory = Persistence.createEntityManagerFactory("MyJpa");
+         entityManager = entityManagerFactory.createEntityManager();
+    }
+    /*
+    Insert new record to database by two parameters "title" and "year".
+     */
+    public void save(MovieModel model){
+        getTransaction(model);
+        System.out.println("New record added.");
+    }
+
+
+
+
+
+    public void getTransaction(MovieModel model){
+        entityManager.getTransaction().begin();
+        entityManager.persist(model);
+        entityManager.getTransaction().commit();
+
+    }
+
+    public void closeConnection(){
+        entityManagerFactory.close();
+        entityManager.close();
+    }
+}
+
+
